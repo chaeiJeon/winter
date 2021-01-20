9fdbfae0a0b082531b2be102f42b071a6ab05e2a
jeonchaei <tgb09876@naver.com>
2021-01-20 17:50:00 +0900

    01

--- pages/head.js
+++ pages/head.js
@@ -1,9 +1,23 @@
-const Head = () => {
+import { Calendar } from 'antd';
+import { Row, Col } from "antd";
+const Head = ({children}) => {
     return (
         <div>
-            head
+            <Row gutter={18}>
+                {/* gutter은 25% 50% 25% 그 사이 간격주기 */}
+                <Col xs={24} md={6}>
+                    <img src="/images/front.png"/>
+                </Col>
+                <Col xs={24} md={8}>
+                    {children}
+                </Col>
+                <Col xs={24} md={10}>
+                    <a href="https://blog.naver.com/tgb09876" target="_blank" rel="noreferrer noopener">채뚱</a>
+                    <Calendar style={{marginTop:20}}/>
+               </Col>
+            </Row>
         </div>
     );
 
 };
-export default Head;
+export default Head
